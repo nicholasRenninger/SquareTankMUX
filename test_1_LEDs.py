db8ff18e44a7e94b148f964398721314ab47510d
Your Name <you@example.com>
2017-05-17 16:04:06 -0600

    fixed indexing in pin assignment

--- test_1_LEDs.py
+++ test_1_LEDs.py
@@ -21,7 +21,7 @@ while(1):
 	except NameError:
 		pass;
 		
-	selectedLED = input("Enter the LED to Light");
+	selectedLED = input("Enter the LED to Light: ");
 	
 	# address will be a binary string, which will be sent to the GPIO
 	# pins succesively
