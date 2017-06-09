12af34095dc20f89581eaa93ab74881c2e29dacd
Your Name <you@example.com>
2017-06-09 17:57:40 -0600

    added windows virtualenv support

---
+++ Windows/pyAnalogueMUX/test_1_LEDs.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+import math
+
+# Define pins to use
+pinA = 4;
+pinB = 26;
+OUT_PINS = [pinB, pinA];
+MAX_NUM_ADDRESSES = 4; # CD4052
+numAddressBits = int(math.ceil( math.log(MAX_NUM_ADDRESSES) / math.log(2) ));
+
+# Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# setup all of the pins
+for GPIO_Pin in OUT_PINS:
+	GPIO.setup(GPIO_Pin, GPIO.OUT)
+
+### Blink LEDs
+while(1):
+
+	# Get user input
+	try:
+		input = raw_input;
+	except NameError:
+		pass;
+		
+	# don't take the 0b portion of 0b101...
+	selectedLED = int(input("Enter the LED to Light: "));
+	#~ print selectedLED
+	#~ print numAddressBits
+	
+	# address will be a binary string, which will be sent to the GPIO
+	# pins succesively
+	LEDAddress = '%0*d' % (numAddressBits, int(bin(selectedLED)[2:]));
+	#~ print LEDAddress
+	
+	# write address to the GPIO pins
+	for idx, currentPin in enumerate(LEDAddress):
+		print "current pin = ", currentPin
+	    #~ print idx, currentPin
+		# Setup the MUX
+		if (currentPin == "1"):
+			GPIO.output(OUT_PINS[idx], GPIO.HIGH)
+			print OUT_PINS[idx], "is high"
+		else:
+			# currentPin = 0
+			print OUT_PINS[idx], "is low"
+			GPIO.output(OUT_PINS[idx], GPIO.LOW)
+			
+	
+	print("LED on");
+
