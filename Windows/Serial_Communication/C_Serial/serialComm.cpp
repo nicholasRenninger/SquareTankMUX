01455a1352835666fc19e7c6bf65c6d44bb275e7
Your Name <you@example.com>
2017-08-03 10:49:09 -0600

    Added windows serial comm updates and error-handling

--- Windows/Serial_Communication/C_Serial/serialComm.cpp
+++ Windows/Serial_Communication/C_Serial/serialComm.cpp
@@ -4,33 +4,34 @@
 #include <conio.h>
 #include <iostream>
 
-
+#define READ_BUFFER_SIZE 100
+#define WRITE_BUFFER_SIZE 100
 
 
 int main(int argc, char *argv[])
 {
-   int n=0;
-   DCB dcb={0};
+   
+   DCB dcb = {0};
    HANDLE hCom;
    BOOL fSuccess;
-   char *comPortNumber[1];
+   char comPortNumber[1];
    std::cout << "Name of COM port number: ";
-   std::cin >> comPortNumber[1];
-   std::cout << "here" << std::endl;
+   scanf("%s", comPortNumber);
    char pcCommPort[256];
    snprintf(pcCommPort, sizeof(pcCommPort), "%s%s", "COM", comPortNumber);
    //~ char *pcCommPort = "COM2";
-   char szBuff[40]={0};
-   char otherBuff[40]={"300<CR>"};
+   char readBuff[READ_BUFFER_SIZE] = {0};
+   char writeBuff[WRITE_BUFFER_SIZE] = {"@254PR1?;FF"};
    DWORD dwBytesRead=0;
-   DWORD dwBytesWrite=0;
+   DWORD dwBytesWrite=0;  
+   
 /***************************************CommTimeouts******************************************/
-COMMTIMEOUTS timeouts={0};
-timeouts.ReadIntervalTimeout=50;
-timeouts.ReadTotalTimeoutConstant=50;
-timeouts.ReadTotalTimeoutMultiplier=10;
-timeouts.WriteTotalTimeoutConstant=50;
-timeouts.WriteTotalTimeoutMultiplier=10;
+COMMTIMEOUTS timeouts = {0};
+timeouts.ReadIntervalTimeout = 50;
+timeouts.ReadTotalTimeoutConstant = 50;
+timeouts.ReadTotalTimeoutMultiplier = 10;
+timeouts.WriteTotalTimeoutConstant = 50;
+timeouts.WriteTotalTimeoutMultiplier = 10;
 
 
 
@@ -48,7 +49,7 @@ timeouts.WriteTotalTimeoutMultiplier=10;
 /***************************************SET*UP*COM*PORT**************************************/
    if (hCom == INVALID_HANDLE_VALUE)
    {
-       printf ("CreateFile failed with error %d.\n", GetLastError());
+       printf ("CreateFile failed with error %Iu.\n", GetLastError()); // %Iu is for long unsigned int
        return (1);
    }
 
@@ -62,20 +63,20 @@ timeouts.WriteTotalTimeoutMultiplier=10;
    if (!fSuccess)
    {
      /*More Error Handling*/
-      printf ("GetCommState failed with error %d.\n", GetLastError());
+      printf ("GetCommState failed with error %Iu.\n", GetLastError());
       return (2);
    }
 
 
    dcb.BaudRate = 9600;     // set the baud rate
-   dcb.ByteSize = 7;             // data size, xmit, and rcv
-   dcb.Parity = EVENPARITY;        // no parity bit
+   dcb.ByteSize = 8;             // data size, xmit, and rcv
+   dcb.Parity = NOPARITY;        // no parity bit
    dcb.StopBits = ONESTOPBIT;    // one stop bit
    fSuccess = SetCommState(hCom, &dcb);
 
    if (!fSuccess)
    {
-      printf ("SetCommState failed. Error: %d.\n", GetLastError());
+      printf ("SetCommState failed. Error: %Iu.\n", GetLastError());
       return (3);
    }
 
@@ -84,24 +85,35 @@ timeouts.WriteTotalTimeoutMultiplier=10;
 
 /*************************************Reading************************************************/
 
-if(ReadFile(hCom, szBuff, 39, &dwBytesRead, NULL)){
-   int j=0;
-   for(j = 0; j < dwBytesRead; j++){
-     printf("%c", szBuff[j]);
+if(ReadFile(hCom, readBuff, READ_BUFFER_SIZE, &dwBytesRead, NULL)){
+   
+   for(unsigned int j = 0; j < sizeof(readBuff); j++){
+     printf("%c", readBuff[j]);
    }
-   puts("\n"); // Print a new line after printing all the string.
+   puts("\nRead from the Device\n"); // Print a new line after printing all the string.
+} else {
+
+	printf("\nerror reading from device\n");
 }
 
-if(WriteFile(hCom, otherBuff, 39, &dwBytesWrite, NULL)){
-int i=0;
-for(i=0;i<sizeof(otherBuff);i++){
-    printf("%c", otherBuff[i]);
+
+if(WriteFile(hCom, writeBuff, WRITE_BUFFER_SIZE, &dwBytesWrite, NULL)){
+
+for(unsigned int i=0;i<sizeof(writeBuff);i++){
+    printf("%c", writeBuff[i]);
 }
-//printf("The size of dwBytesWrite is: %c", &otherBuff);
-//printf("I wrote to the device\n");
-//printf("I");
+printf("\nThe size of the Write Buffer is: %d\n", sizeof(writeBuff) - 1);
+printf("Wrote to the device\n\n");
 }
-//printf("Heres szBuff %d\n", szBuff);
+
+if(ReadFile(hCom, readBuff, READ_BUFFER_SIZE, &dwBytesRead, NULL)){
+ 
+   for(unsigned int j = 0; j < sizeof(readBuff); j++){
+     printf("%c", readBuff[j]);
+   }
+   puts("\nRead from the Device\n"); // Print a new line after printing all the string.
+}
+
 
 /********************************************************************************************/
 CloseHandle(hCom);
