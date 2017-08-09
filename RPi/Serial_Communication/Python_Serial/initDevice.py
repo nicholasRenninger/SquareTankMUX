53c6786d1da30e94aa62c417b500290cfea05e22
Your Name <you@example.com>
2017-08-08 18:18:02 -0600

    Added support for raspberry pi (ARMv6 architecture).

---
+++ RPi/Serial_Communication/Python_Serial/initDevice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# imports
+import serial
+
+
+def initializePort(portname):
+
+    # create serial port object
+    serPort = serial.Serial()
+
+    # setup the serial port with the passed portname e.g. COM5
+    serPort.port = portname
+
+    serPort.baudrate = 9600  # max bits per sec transf speed
+    serPort.bytesize = serial.EIGHTBITS  # number of bits per bytes
+    serPort.parity = serial.PARITY_NONE  # set parity check: no parity
+    serPort.stopbits = serial.STOPBITS_ONE  # number of stop bits
+    serPort.timeout = 0  # non-block read
+    serPort.xonxoff = False  # disable software flow control
+    serPort.rtscts = False  # disable hardware (RTS/CTS) flow control
+    serPort.dsrdtr = False  # disable hardware (DSR/DTR) flow control
+    serPort.writeTimeout = 0  # timeout for write
+
+    return serPort
