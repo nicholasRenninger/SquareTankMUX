387d2a8dac12862f8bf4e7bf54048f906de36cb9
Your Name <you@example.com>
2018-03-09 11:24:51 -0700

    Testing setup.py functionality with multiple guages from set of valid devices

--- Ubuntu/Python_Serial/setup.py
+++ Ubuntu/Python_Serial/setup.py
@@ -9,6 +9,9 @@ from readMKSGauge import readGauge
 # set the command to request device type data from gauge
 pressCMD = "@254DT?;FF"
 
+# set the command to request serial number from gauge
+pressCMD = "@254SN?;FF"
+
 # valid Device Types
 VALID_DEVICES = ("MICROPIRANI", "DUALMAG")
 
@@ -16,9 +19,15 @@ VALID_DEVICES = ("MICROPIRANI", "DUALMAG")
 comport = "0"
 comportIDX = 0
 rate = 1
+validPortList = []
 
 
 def setupGuagePort():
+
+    """Returns list of valid device serial ports.
+
+    Note: The list of valid devices is given by a text file, which contains
+    """
 
     # Find Live Ports
     ports = list(portz.comports())
@@ -46,20 +55,25 @@ def setupGuagePort():
             except Exception as e:
 
                 print("error opening serial port: ", str(e))
-                return ""
 
             # If opening the gauge was successful, determine if the port is an
             # MKS gauge. If it is, return the serial port object.
             shouldPrint = True
             (errorSTR, guageData) = readGauge(serPort, pressCMD, shouldPrint,
                                               rate)
+            print(guageData)
 
             # now check if the device sends a valid device type back
             if guageData in VALID_DEVICES:
                 print('Using', p.device)
-                return serPort
+                validPortList.append(serPort)
 
-        # if no valid devices are found in the entire list, then print an error
-        print ('Did not find valid MKS gauge attached.',
-               'Check connections and try again.')
+        if not validPortList:
+            return validPortList
+        else:
+            # if no valid devices are found in the entire list, then print an
+            # error
+            print ('Did not find valid MKS gauge attached.',
+                   'Check connections and try again.')
+
         return ""
