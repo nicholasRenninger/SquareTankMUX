82a352a963fb290648deb34ee9575c12e7de98ec
nicholasRenninger <nire1188@colorado.edu>
2018-04-16 11:19:56 -0600

    cleaning up wait time and mux address

--- RPi/Python_Serial/meas_device.py
+++ RPi/Python_Serial/meas_device.py
@@ -139,11 +139,11 @@ class meas_device:
                   self.meas_units
         :rtype: string (UTF-8)
         """
+        print(self.num_start_chars)
 
         # remove addressing and termination from data
-
-        # get rid of everything before error message
         data = raw_data[self.num_start_chars:]
+
         # cut off termination
         data = data[:-(len(self.term_char))]
 
