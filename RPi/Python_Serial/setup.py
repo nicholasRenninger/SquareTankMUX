7fcd0c0fdab40caccff7297852815e80bc181c50
nicholasRenninger <nire1188@colorado.edu>
2018-04-27 12:54:09 -0600

    bogo-debugging

--- RPi/Python_Serial/setup.py
+++ RPi/Python_Serial/setup.py
@@ -224,7 +224,7 @@ def setGPIOPins(GPIO_out_pins, GPIO_in_pins):
     """
 
     # Setup
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
     # setup all of the pins to be digital in / out
